b871bf029b13b34b3bb7f3dbc5a32268bf767095
javierbeltran <40900147+javierbeltran@users.noreply.github.com>
2020-04-09 21:02:40 -0400

    Add files via upload

---
+++ SistemaFacturacion/SistemaFacturacion/App_Data/TipoFacturacion.vb
@@ -0,0 +1,69 @@
+﻿Public Class TipoFacturacion
+    Private _NombreProducto As String
+    Private _Fecha As DateTime
+    Private _Total As Decimal
+    Private _Cantidad As Integer
+    Private _Codigo As Integer
+    Private _Precio As Integer
+
+    Public Property Codigo() As Integer
+        Get
+            Return _Codigo
+        End Get
+        Set(ByVal value As Integer)
+            _Codigo = value
+        End Set
+    End Property
+    Public Property NombreProducto() As String
+        Get
+            Return _NombreProducto
+        End Get
+        Set(ByVal value As String)
+            _NombreProducto = value
+        End Set
+
+    End Property
+    Public Property Fecha() As DateTime
+        Get
+            Return _Fecha
+        End Get
+        Set(ByVal value As DateTime)
+            _Fecha = value
+        End Set
+
+    End Property
+    Public Property Total() As Decimal
+        Get
+            Return _Total
+        End Get
+        Set(ByVal value As Decimal)
+            _Total = value
+        End Set
+    End Property
+
+    Public Property Cantidad() As Integer
+        Get
+            Return _Cantidad
+        End Get
+        Set(ByVal value As Integer)
+            _Cantidad = value
+        End Set
+    End Property
+
+    Public Property Precio() As Integer
+        Get
+            Return _Precio
+        End Get
+        Set(ByVal value As Integer)
+            _Precio = value
+        End Set
+    End Property
+    Public Sub New()
+        Me.Cantidad = 0
+        Me.Total = 0
+        Me.Codigo = 0
+        Me.Fecha = Date.Now
+        Me.Precio = 0
+
+    End Sub
+End Class
---
+++ SistemaFacturacion/SistemaFacturacion/App_Data/TipoProducto.vb
@@ -0,0 +1,60 @@
+﻿Friend Class TipoProducto
+
+    Private _NombreProducto As String
+    Private _Categoria As String
+    Private _Precio As Integer
+    Private _Codigo As Integer
+    Private _Cantidad As Integer
+    Public Property Cantidad() As Integer
+        Get
+            Return _Cantidad
+        End Get
+        Set(ByVal value As Integer)
+            _Cantidad = value
+        End Set
+    End Property
+
+    Public Property Codigo() As Integer
+        Get
+            Return _Codigo
+        End Get
+        Set(ByVal value As Integer)
+            _Codigo = value
+        End Set
+    End Property
+
+    Public Property NombreProducto() As String
+        Get
+            Return _NombreProducto
+        End Get
+        Set(ByVal value As String)
+            _NombreProducto = value
+        End Set
+
+    End Property
+    Public Property Categoria() As String
+        Get
+            Return _Categoria
+        End Get
+        Set(ByVal value As String)
+            _Categoria = value
+        End Set
+    End Property
+
+    Public Property Precio() As Integer
+        Get
+            Return _Precio
+        End Get
+        Set(ByVal value As Integer)
+            _Precio = value
+        End Set
+    End Property
+
+    Public Sub New()
+        Me.Categoria = String.Empty
+        Me.NombreProducto = String.Empty
+        Me.Precio = 0
+        Me.Codigo = 0
+        Me.Cantidad = 0
+    End Sub
+End Class
